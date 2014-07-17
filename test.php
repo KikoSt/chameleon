ec325fb2d284d2b2c0d042a1ca01932f69c64406
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-07-17 13:24:39 +0200

    merge testfile

--- test.php
+++ test.php
@@ -1,5 +1,7 @@
 <?php
 
+$myContainer = new GfxContainer();
+
 $color = new GfxColor();
 $color->setColorHex('#1Z3fFZ');
 echo ($color->getColorHex());
@@ -7,6 +9,15 @@ echo ($color->getColorHex());
 $test = new GfxRectangle();
 $test->setPosition(10, 200);
 $test->create();
+$t1 = new GfxRectangle();
+$t2 = new GfxRectangle();
+$t1->setPosition(10, 200);
+$t2->setPosition(123, 815);
+
+$myContainer->addElement($t1);
+$myContainer->addElement($t2);
+
+
 
 function __autoload($className) {
     if(file_exists('classes/' . $className . '.class.php')) {
