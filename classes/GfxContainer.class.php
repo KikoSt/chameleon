3fb22c1276e5ac3fb9ed24582639d5df9a76c268
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-07-18 15:10:00 +0200

    cleanup

--- classes/GfxContainer.class.php
+++ classes/GfxContainer.class.php
@@ -12,10 +12,113 @@ class GfxContainer // implements Renderable ;)
     private $sId;
     protected $elements;
     private $target;
+    private $canvasWidth;
+    private $canvasHeight;
+
+    private $allowedTargets;
 
     public function __construct()
     {
         $this->elements = array();
+        $this->allowedTargets = array('SWF', 'GIF');
+    }
+
+    public function addElement($element)
+    {
+        if(is_a($element, 'GfxComponent')) {
+            $this->elements[] = $element;
+        } else {
+        }
+    }
+
+
+
+
+    public function render()
+    {
+        if($this->target === 'SWF') {
+            $this->renderSWF();
+        } else if($this->target === 'GIF') {
+            $this->renderGIF();
+        }
+    }
+
+    private function renderSWF()
+    {
+        $fonts = array();
+        $swf = new SWFMovie();
+        $swf->setDimension($this->getCanvasWidth(), $this->getCanvasHeight());
+        $swf->setFrames(30);
+        $swf->setRate(10);
+        $swf->setBackground(0, 0, 0);
+
+        $fonts['normal'] = new SWFFont('fdb/bvs.fdb');
+
+        $count = 0;
+        $texts = array();
+
+        foreach($this->elements AS $element) {
+            if(is_a($element, 'GfxRectangle') || is_a($element, 'GfxText')) {
+                $element->renderSWF($swf);
+            }
+        }
+        $swf->save('output.swf');
+
+    }
+
+    private function renderGIF()
+    {
+    }
+
+
+
+
+
+
+
+
+
+
+    /* **************************************
+              Accessors
+    ***************************************** */
+    public function setTarget($target)
+    {
+        if(!in_array($target, $this->allowedTargets)) {
+            throw new Exception('Unknown format ' . $target . ' for GfxContainer');
+        } else {
+            $this->target = $target;
+        }
+    }
+
+    public function getTarget()
+    {
+        return $this->target;
+    }
+
+    public function getCanvasWidth()
+    {
+        return $this->canvasWidth;
+    }
+
+    public function getCanvasHeight()
+    {
+        return $this->canvasHeight;
+    }
+    public function setCanvasWidth($newCanvasWidth)
+    {
+        $this->canvasWidth = $newCanvasWidth;
+    }
+
+    public function setCanvasHeight($newCanvasHeight)
+    {
+        $this->canvasHeight = $newCanvasHeight;
+    }
+
+    public function setCanvasSize($newCanvasWidth, $newCanvasHeight)
+    {
+        $this->canvasWidth = $newCanvasWidth;
+        $this->canvasHeight = $newCanvasHeight;
     }
 
     public function setId($sId)
@@ -28,35 +131,8 @@ class GfxContainer // implements Renderable ;)
         return $this->sId;
     }
 
-    public function addElement($element)
-    {
-        if(is_a($element, 'GfxComponent')) {
-            $this->elements[] = $element;
-            echo 'Success!';
-        } else {
-            echo 'No!';
-        }
-    }
 
-    public function render()
-    {
-
-    }
-
-    public function setTarget($target)
-    {
-        if(!in_array($target, $this->allowedTargets)) {
-            die('What a terrible death!');
-        } else {
-            $this->target = $target;
-        }
-    }
-
-    public function getTarget()
-    {
-        return $this->target;
-    }
-
+    // Magic Methods
     public function __toString()
     {
         $string = '';
@@ -65,4 +141,7 @@ class GfxContainer // implements Renderable ;)
         }
         return $string;
     }
+
+
+
 }
