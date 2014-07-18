d6bf00a5e4ee4890c3d93329627ea363b8818523
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-07-18 15:48:34 +0200

    output/test changes

--- swf_test.php
+++ swf_test.php
@@ -27,19 +27,37 @@ $r2->setHeight(475);
 $r1->setColor($c1);
 $r2->setColor($c2);
 
-$t1 = new GfxText($c1);
+$t1 = new GfxText();
 $t1->setId("headline");
-$t1->setText("das ist ein toller text");
+$t1->setText("Globetrotter");
 $t1->setFont(new SWFFont('fdb/bvs.fdb'));
 $t1->setColor($c3);
 $t1->setHeight(10);
 $t1->setPosition(($canvasWidth / 2) - ($t1->getTextWidth() / 2), $canvasHeight - 25);
+
+$t2 = new GfxText();
+$t2->setId("tagline1");
+$t2->setText("get active");
+$t2->setFont(new SWFFont('fdb/bvs.fdb'));
+$t2->setColor($c1);
+$t2->setHeight(20);
+$t2->setPosition(($canvasWidth / 2) - ($t2->getTextWidth() / 2), $canvasHeight - 175);
+
+$t3 = new GfxText();
+$t3->setId("tagline2");
+$t3->setText("NOW!");
+$t3->setFont(new SWFFont('fdb/bvs.fdb'));
+$t3->setColor($c3);
+$t3->setHeight(28);
+$t3->setPosition(($canvasWidth / 2) - ($t3->getTextWidth() / 2), $canvasHeight - 125);
 
 $myContainer->setCanvasSize($canvasWidth, $canvasHeight);
 
 $myContainer->addElement($r1);
 $myContainer->addElement($r2);
 $myContainer->addElement($t1);
+$myContainer->addElement($t2);
+$myContainer->addElement($t3);
 // END OF CONTAINER CODE
 
 
