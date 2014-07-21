fa8599de5d7d442955793af6ed0320dcd8866d10
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-07-21 11:07:36 +0200

    remove parent constructor call from container since container has no parent :P

--- classes/GfxContainer.class.php
+++ classes/GfxContainer.class.php
@@ -20,7 +20,6 @@ class GfxContainer
 
     public function __construct()
     {
-        parent::__construct();
         $this->elements = array();
         $this->allowedTargets = array('SWF', 'GIF');
     }
