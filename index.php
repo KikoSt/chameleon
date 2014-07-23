cc79c6d8c8be3503f136b0efdf1ed2dce0bb179f
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-07-23 11:02:17 +0200

    change structure - for good, hopefully :)

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+// TODO: this will be _POST later; _REQUEST for development only
+$page = $_REQUEST['page'];
+
+$modules = array('test');
+
+if(in_array($page, $modules)) {
+    //
+}
+
+?>
