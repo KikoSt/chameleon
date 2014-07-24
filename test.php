89917ed94bc79181128aa1456d64f0aeacae728c
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-07-24 14:33:57 +0200

    demo changes

--- test.php
+++ test.php
@@ -1,12 +1,16 @@
 <?php
 
-$myContainer = new GfxContainer();
-$myContainer->setSource('svg/ttest_4.svg');
-$myContainer->parse();
-$myContainer->setTarget('SWF');
-$myContainer->render();
-$myContainer->setTarget('GIF');
-$myContainer->render();
+for($i=1; $i<5;$i++)
+{
+    $myContainer = new GfxContainer();
+    $myContainer->setSource('svg/ttest_' . $i . '.svg');
+    $myContainer->setOutputName('output_' . $i);
+    $myContainer->parse();
+    $myContainer->setTarget('SWF');
+    $myContainer->render();
+    $myContainer->setTarget('GIF');
+    $myContainer->render();
+}
 
 function __autoload($className) {
     if(file_exists('libraries/classes/' . $className . '.class.php')) {
