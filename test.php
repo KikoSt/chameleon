861350301661a9c243aed03fce34536b6722da49
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-07-29 16:53:36 +0200

    add testing code for REST API

--- test.php
+++ test.php
@@ -1,5 +1,21 @@
 <?php
 
+$rest = new RESTCenter();
+
+$template = new BannerTemplateModel('svg/ttest_1.svg');
+$template->setDescription('first dummy test thingie');
+$template->setIdBannerTemplate(null);
+$template->setIdParentBannerTemplate(null);
+$template->setName('dummy');
+$template->setIdAdvertiser(122);
+$template->setIdAuditUser(null);
+$template->setSvgContent(simplexml_load_string('<svg><g><title>test</title></g></svg>')->asXml());
+
+$rest->getTemplates(122);
+$rest->sendBannerTemplate($template);
+
+exit(0);
+
 for($i=1; $i<6;$i++)
 {
     $myContainer = new GfxContainer();
