2f7fa7c37e63aa138ddac0b43b133d1ea40984b4
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-07-30 17:30:40 +0200

    add small fixes to have the test.php file running with the changes as well

--- test.php
+++ test.php
@@ -1,4 +1,11 @@
 <?php
+require_once('Bootstrap.php');
+include('config/pathconfig.inc.php');
+
+if(!defined('__ROOT__'))
+{
+    define('__ROOT__', './');
+}
 
 for($i=1; $i<6;$i++)
 {
@@ -21,10 +28,10 @@ $myContainer->render();
 $myContainer->setTarget('GIF');
 $myContainer->render();
 
-function __autoload($className) {
-    if(file_exists('libraries/classes/' . $className . '.class.php')) {
-        require_once('libraries/classes/' . $className . '.class.php');
-    } else if(file_exists('libraries/interfaces/' . $className . '.interface.php')) {
-        require_once('libraries/interfaces/' . $className . '.interface.php');
-    }
-}
+// function __autoload($className) {
+//     if(file_exists('libraries/classes/' . $className . '.class.php')) {
+//         require_once('libraries/classes/' . $className . '.class.php');
+//     } else if(file_exists('libraries/interfaces/' . $className . '.interface.php')) {
+//         require_once('libraries/interfaces/' . $className . '.interface.php');
+//     }
+// }
