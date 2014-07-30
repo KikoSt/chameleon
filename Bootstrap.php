45d5a5c55021df483037f64861e563e5e3bb50ee
Thomas Hummel <thomas.hummel@mediadecision.com>
2014-07-30 11:19:20 +0200

    path workaround

---
+++ Bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @param $className
+ */
+function __autoload($className)
+{
+    if(file_exists(__ROOT__ . 'libraries/classes/' . $className . '.class.php'))
+    {
+        require_once(__ROOT__ . 'libraries/classes/' . $className . '.class.php');
+    }
+    else if(file_exists(__ROOT__ . 'libraries/interfaces/' . $className . '.interface.php'))
+    {
+        require_once(__ROOT__ . 'libraries/interfaces/' . $className . '.interface.php');
+    }
+    else if(file_exists(__ROOT__ . 'libraries/exception/' . $className . '.exception.php'))
+    {
+        require_once(__ROOT__ . 'libraries/exception/' . $className . '.exception.php');
+    }
+    else if(file_exists(__ROOT__ . 'controllers/' . $className . '.php'))
+    {
+        require_once(__ROOT__ . 'controllers/' . $className . '.php');
+    }
+}
--- config/pathconfig.inc.php
+++ config/pathconfig.inc.php
@@ -1,9 +1,20 @@
 <?php
 
-define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/');
+if(!isset($_SERVER['HTTP_HOST']))
+{
+    // called locally via console!
+    $basedir = '/var/www/';
+}
+else
+{
+    $basedir = $_SERVER['DOCUMENT_ROOT'] . '/';
+}
+
+define('ROOT_DIR', $basedir . 'chameleon/');
+
 
 define('ASSET_DIR', ROOT_DIR . 'assets/');
-define('OUTPUT_DIR', ROOT_DIR . '/output/');
+define('OUTPUT_DIR', ROOT_DIR . 'output/');
 define('AJAX_DIR', ROOT_DIR . 'ajax/');
 define('CONFIG_DIR', ROOT_DIR . 'config/');
 define('CONTROLLER_DIR', ROOT_DIR . 'controllers/');
