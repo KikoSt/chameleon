1c3f75a339cad57c4cc4ed387794f6724fb72c4a
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-08-04 18:25:19 +0200

    add some more accessors and PHPDoc style comments

--- libraries/classes/GfxAnimation.class.php
+++ libraries/classes/GfxAnimation.class.php
@@ -15,24 +15,73 @@ class GfxAnimation
         $this->numFrames = 0;
     }
 
+    /**
+     * setframeRate
+     *
+     * @param int $frameRate the framerate for this animation
+     * @access public
+     * @return void
+     */
     public function setframeRate($frameRate)
     {
         $this->frameRate = $frameRate;
     }
 
+    /**
+     * getFrameRate
+     *
+     * @access public
+     * @return int frameRate the framerate for this animation
+     */
+     // TODO: not necessarily ... the framerate will most likely be defined globally by the movie itself,
+     // components cannot have differen framerates!
     public function getFrameRate()
     {
         return $this->frameRate;
     }
 
+    /**
+     * setNumFrames
+     *
+     * @param mixed $numFrames
+     * @param int numFrames the number of frames the current animation should last
+     * @access public
+     * @return void
+     */
     public function setNumFrames($numFrames)
     {
         $this->numFrames = $numFrames;
     }
 
-    public function getNumFames()
+    /**
+     * getNumFrames
+     *
+     * @access public
+     * @return numFrames int the number of frames the current animation should last
+     */
+    public function getNumFrames()
     {
         return $this->numFrames;
+    }
+
+    /**
+     * Get targetComponent.
+     *
+     * @return targetComponent.
+     */
+    public function getTargetComponent()
+    {
+        return $this->targetComponent;
+    }
+
+    /**
+     * Set targetComponent.
+     *
+     * @param targetComponent the value to set.
+     */
+    public function setTargetComponent($targetComponent)
+    {
+        $this->targetComponent = $targetComponent;
     }
 }
 
