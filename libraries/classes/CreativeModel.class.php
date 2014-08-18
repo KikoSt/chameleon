e0976ce096b1f5fabcbc5492f9e4f3aad69d20c0
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-08-18 15:40:33 +0200

    enhance and clean up the CreativeModel

--- libraries/classes/CreativeModel.class.php
+++ libraries/classes/CreativeModel.class.php
@@ -2,11 +2,43 @@
 
 class CreativeModel
 {
-    private $width;
-    private $height;
+    private $dimX;
+    private $dimY;
     private $swfPath;
     private $gifPath;
-    private $productId;
+    private $swfFileName;
+    private $gifFileName;
+    private $swfContent;
+    private $gifContent;
+    private $idProduct;
+
+
+    public function __construct()
+    {
+    }
+
+
+
+    public function prepareTransmission()
+    {
+        // check if information on both files are available AND both files are accessible
+        if(empty($this->swfPath) || !($handle = fopen($this->getSwfPath(), 'r')))
+        {
+            throw new Exception('No information on swf file provided');
+        }
+        fclose($handle);
+        if(empty($this->gifPath) || !($handle = fopen($this->getGifPath(), 'r')))
+        {
+            throw new Exception('No information on gif file provided');
+        }
+        fclose($handle);
+
+        $this->setSwfBin(base64_encode(file_get_contents($this->getSwfPath())));
+        $this->setGifBin(base64_encode(file_get_contents($this->getGifPath())));
+
+        // $this->setSwfBin(base64_encode('')); // base64_encode(file_get_contents($this->getSwfPath())));
+        // $this->setGifBin(base64_encode('')); // base64_encode(file_get_contents($this->getGifPath())));
+    }
 
 
     /**
@@ -16,7 +48,7 @@ class CreativeModel
      */
     public function getWidth()
     {
-        return $this->width;
+        return $this->dimX;
     }
 
     /**
@@ -26,7 +58,7 @@ class CreativeModel
      */
     public function setWidth($width)
     {
-        $this->width = $width;
+        $this->dimX = $width;
     }
 
     /**
@@ -36,7 +68,7 @@ class CreativeModel
      */
     public function getHeight()
     {
-        return $this->height;
+        return $this->dimY;
     }
 
     /**
@@ -46,7 +78,7 @@ class CreativeModel
      */
     public function setHeight($height)
     {
-        $this->height = $height;
+        $this->dimY = $height;
     }
 
     /**
@@ -96,7 +128,7 @@ class CreativeModel
      */
     public function getProductId()
     {
-        return $this->productId;
+        return $this->idProduct;
     }
 
     /**
@@ -106,7 +138,93 @@ class CreativeModel
      */
     public function setProductId($productId)
     {
-        $this->productId = $productId;
+        $this->idProduct = $productId;
+    }
+
+    /**
+     * Get swfBin.
+     *
+     * @return swfBin.
+     */
+    public function getSwfBin()
+    {
+        return $this->swfContent;
+    }
+
+    /**
+     * Set swfBin.
+     *
+     * @param swfBin the value to set.
+     */
+    public function setSwfBin($swfBin)
+    {
+        $this->flashContent = $swfBin;
+    }
+
+    /**
+     * Get gifBin.
+     *
+     * @return gifBin.
+     */
+    public function getGifBin()
+    {
+        return $this->gifContent;
+    }
+
+    /**
+     * Set gifBin.
+     *
+     * @param gifBin the value to set.
+     */
+    public function setGifBin($gifBin)
+    {
+        $this->gifContent = $gifBin;
+    }
+
+    public function setFileName($filename)
+    {
+        $this->setSwfFilename($filename . '.swf');
+        $this->setGifFilename($filename . '.gif');
+    }
+
+    /**
+     * Get swfFileName.
+     *
+     * @return swfFileName.
+     */
+    public function getSwfFilename()
+    {
+        return $this->swfFileName;
+    }
+
+    /**
+     * Set swfFileName.
+     *
+     * @param swfFileName the value to set.
+     */
+    private function setSwfFilename($swfFilename)
+    {
+        $this->swfFileName = $swfFilename;
+    }
+
+    /**
+     * Get gifFileName.
+     *
+     * @return gifFileName.
+     */
+    public function getGifFilename()
+    {
+        return $this->gifFileName;
+    }
+
+    /**
+     * Set gifFileName.
+     *
+     * @param gifFileName the value to set.
+     */
+    private function setGifFilename($gifFilename)
+    {
+        $this->gifFileName = $gifFilename;
     }
 }
 
