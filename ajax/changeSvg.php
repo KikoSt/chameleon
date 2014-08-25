f278b85021dd3f3d0aa5a4ff9f78521831f6d3e0
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-08-25 14:46:11 +0200

    clean up ajax script changeSvg

--- ajax/changeSvg.php
+++ ajax/changeSvg.php
@@ -18,40 +18,40 @@ if(!defined('__ROOT__'))
 require_once(__ROOT__ . 'libraries/functions.inc.php');
 
 $container = new GfxContainer();
-$connector = new APIConnector();
 $svgHandler = new SvgFileHandler();
 
 // for now ...
 $auditUserId = 14;
 
-$bannerTemplateId = getRequestVar('templateId');
+$companyId    = getRequestVar('companyId');
+$advertiserId = getRequestVar('advertiserId');
+$templateId   = getRequestVar('templateId');
 
-$container->setCompanyId(getRequestVar('companyId'));
-$container->setAdvertiserId(getRequestVar('advertiserId'));
+$container->setCompanyId($companyId);
+$container->setAdvertiserId($advertiserId);
+
+$basePath = (string) $companyId . '/' . (string) $advertiserId . '/';
 
 if(!empty($_FILES))
 {
     foreach($_FILES as $singleFile)
     {
         $filename = ASSET_DIR . $singleFile['name'];
-        echo $filename;
         move_uploaded_file($singleFile['tmp_name'], $filename);
     }
 }
 
-$template = $connector->getTemplateById($bannerTemplateId);
-
-$connector->setCompanyId(getRequestVar('companyId'));
-$connector->setAdvertiserId(getRequestVar('advertiserId'));
+// TODO: The template should be known and kept locally right now ...
+// $template = $connector->getTemplateById($templateId);
 
 //set file name
-$baseFilename = 'rtest_' . $bannerTemplateId;
+$baseFilename = 'rtest_' . $templateId;
 $filename = $baseFilename . '.svg';
 $container->setOutputName($baseFilename);
-$svgHandler->setFilename($filename);
+$svgHandler->setFilename($basePath . $filename);
 
 //parse the svg
-$container->setSource($filename);
+$container->setSource($basePath . $filename);
 $container->parse();
 
 //create a new svg with the given request parameters
@@ -62,7 +62,6 @@ if(null !== $_FILES && count($_FILES) > 0)
     {
         foreach($_FILES as $key => $singleFile)
         {
-            echo $key . "\n";
             if($key === $element->getId())
             {
                 $element->setImageUrl("assets/" . $singleFile['name']);
@@ -81,11 +80,16 @@ $container->setTarget('GIF');
 $container->render();
 
 // write the temporary file
+// TODO: integrate this into Container!
 $svgHandler->setSvgContent($svgContent);
 $svgHandler->save();
 
 if(array_key_exists('action', $_REQUEST) && 'save' === $_REQUEST['action'])
 {
+    $connector = new APIConnector();
+    $connector->setCompanyId(getRequestVar('companyId'));
+    $connector->setAdvertiserId(getRequestVar('advertiserId'));
+
     //update template in the data base
     $bannerTemplateModel = new BannerTemplateModel();
     $bannerTemplateModel->setSvgContent($svgContent);
@@ -98,12 +102,12 @@ if(array_key_exists('action', $_REQUEST) && 'save' === $_REQUEST['action'])
     $response = $connector->sendBannerTemplate($bannerTemplateModel);
 }
 
-
-
 $response = array();
-$response['imgsrc'] = $container->getOutputDir() . '/' . $container->getOutputName();
 
-
+// $imgsrc = $container->getOutputDir() . '/' . $container->getOutputName() . '.gif';
+// TODO: improve this path handling, too
+$imgsrc = 'output/' . $basePath . '0/' . $container->getOutputName() . '.gif';
+$response['imgsrc'] = $imgsrc;
 
 echo json_encode($response);
 
