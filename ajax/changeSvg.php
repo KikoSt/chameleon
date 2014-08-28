0c758584f4ddeb9e1a4d40b5667500b806fc79b7
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-08-28 17:07:22 +0200

    cleanup and adjust to changed path handling

--- ajax/changeSvg.php
+++ ajax/changeSvg.php
@@ -29,28 +29,24 @@ $templateId   = getRequestVar('templateId');
 $container->setCompanyId($companyId);
 $container->setAdvertiserId($advertiserId);
 
-$basePath = (string) $companyId . '/' . (string) $advertiserId . '/';
+$basePath = (string) $companyId . '/' . (string) $advertiserId . '/0';
 
 if(!empty($_FILES))
 {
     foreach($_FILES as $singleFile)
     {
-        $filename = ASSET_DIR . $singleFile['name'];
+        $filename = ASSET_DIR . '/' . $singleFile['name'];
         move_uploaded_file($singleFile['tmp_name'], $filename);
     }
 }
-
-// TODO: The template should be known and kept locally right now ...
-// $template = $connector->getTemplateById($templateId);
 
 //set file name
 $baseFilename = 'rtest_' . $templateId;
 $filename = $baseFilename . '.svg';
 $container->setOutputName($baseFilename);
-$svgHandler->setFilename($basePath . $filename);
 
 //parse the svg
-$container->setSource($basePath . $filename);
+$container->setSource($filename);
 $container->parse();
 
 // TODO
@@ -73,19 +69,15 @@ if(null !== $_FILES && count($_FILES) > 0)
         {
             if($key === $element->getId())
             {
-                $element->setImageUrl("assets/" . $singleFile['name']);
+                $element->setImageUrl('/assets/' . $singleFile['name']);
             }
         }
     }
 }
+
 $svgContent = $container->createSvg();
 $container->setTarget('GIF');
 $container->render();
-
-// write the temporary file
-// TODO: integrate this into Container!
-$svgHandler->setSvgContent($svgContent);
-$svgHandler->save();
 
 if(array_key_exists('action', $_REQUEST) && 'save' === $_REQUEST['action'])
 {
@@ -112,7 +104,7 @@ $response = array();
 
 // $imgsrc = $container->getOutputDir() . '/' . $container->getOutputName() . '.gif';
 // TODO: improve this path handling, too
-$imgsrc = 'output/' . $basePath . '0/' . $container->getOutputName() . '.gif';
+$imgsrc = 'output/' . $basePath . '/' . $container->getOutputName() . '.gif';
 $response['imgsrc'] = $imgsrc;
 
 echo json_encode($response);
