a9b76d32055048ba512214b6b902149795684412
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-08-28 17:03:50 +0200

    remove trailing slashes from directory constants; It seems better to have a unified structure when conotating stings, so we'll got for ALWAYS adding slashes between elements for now

--- config/pathconfig.inc.php
+++ config/pathconfig.inc.php
@@ -12,17 +12,17 @@ else
 define('ROOT_DIR', $basedir . 'chameleon/');
 
 
-define('ASSET_DIR', ROOT_DIR . 'assets/');
-define('OUTPUT_DIR', ROOT_DIR . 'output/');
-define('AJAX_DIR', ROOT_DIR . 'ajax/');
-define('CONFIG_DIR', ROOT_DIR . 'config/');
-define('CONTROLLER_DIR', ROOT_DIR . 'controllers/');
-define('FONT_FDB_DIR', ROOT_DIR . 'fonts/fdb/');
-define('FONT_TTF_DIR', ROOT_DIR . 'fonts/ttf/');
+define('ASSET_DIR', ROOT_DIR . 'assets');
+define('OUTPUT_DIR', ROOT_DIR . 'output');
+define('AJAX_DIR', ROOT_DIR . 'ajax');
+define('CONFIG_DIR', ROOT_DIR . 'config');
+define('CONTROLLER_DIR', ROOT_DIR . 'controllers');
+define('FONT_FDB_DIR', ROOT_DIR . 'fonts/fdb');
+define('FONT_TTF_DIR', ROOT_DIR . 'fonts/ttf');
 define('IMAGE_DIR', ROOT_DIR . 'images');
-define('CLASS_DIR', ROOT_DIR . 'libraries/classes/');
-define('INTERFACE_DIR', ROOT_DIR . 'libraries/interfaces/');
+define('CLASS_DIR', ROOT_DIR . 'libraries/classes');
+define('INTERFACE_DIR', ROOT_DIR . 'libraries/interfaces');
 define('EXCEPTION_DIR', ROOT_DIR . 'libraries/exception');
-define('SVG_DIR', ROOT_DIR . 'svg/');
+define('SVG_DIR', ROOT_DIR . 'svg');
 
-define('GIFPROTO', ROOT_DIR . 'assets/gifProto/');
+define('GIFPROTO', ROOT_DIR . 'assets/gifProto');
