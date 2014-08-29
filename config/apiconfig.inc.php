28cb109c1b369b2e42aff856026850872c51f9b5
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-08-29 15:55:46 +0200

    add "global" config file that should include ALL other config files

---
+++ config/apiconfig.inc.php
@@ -0,0 +1,6 @@
+<?php
+
+define('REST_API_SERVICE_URL', 'http://bidder.mediadecision.lan:8080/chameleon-0.2/rest');
+define('REST_API_USERNAME', 'chameleon-api');
+define('REST_API_PASSWORD', 'BwHwEpJnIqUgWkOv9YDg');
+
