77bfde792dbf06347e5daf460e5ad33d43a8e2cf
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-08-29 16:34:51 +0200

    cleanup

--- controllers/Editor.php
+++ controllers/Editor.php
@@ -15,8 +15,7 @@ class Editor extends Controller
     public function create()
     {
         $container = new GfxContainer();
-        $connector = new APIConnector();
-        $text = new GfxText($container);
+        $text      = new GfxText($container);
 
         $this->view = $this->setLayout('views/editor.phtml')->getView();
 
@@ -28,19 +27,6 @@ class Editor extends Controller
         $container->setcompanyId($companyId);
         $container->setAdvertiserId($advertiserId);
 
-        // WTF!
-        // No, no, no!
-        // If the id's aren't provided, the script would already have been terminated; Those information MUST
-        // be provided
-        // Information should not be stored based on assumptions but we should know the state and react adequately
-
-        // all three ID's, the templateId, advertiserId AND companyId should be included in EVERY call!
-        // $_SESSION['templateId']   = $templateId;
-        // $_SESSION['advertiserId'] = $advertiserId;
-        // $_SESSION['companyId']    = $companyId;
-
-//        $basePath = (string) $this->getCompanyId() . '/' . (string) $this->getAdvertiserId() . '/' . '0';
-
         $baseFilename = 'rtest_' . $container->getId();
         $filename = $baseFilename . '.svg';
 
@@ -49,39 +35,9 @@ class Editor extends Controller
         $container->setOutputName($baseFilename);
         $container->setSource($filename);
         $container->parse();
+        $container->setPreviewMode(false);
         $container->setTarget('GIF');
         $container->render();
-
-
-
-/*
-        // check if svg_dir exists
-        if(is_dir(SVG_DIR . '/' . $basePath))
-        {
-            // prepare the file name
-
-            // check if file with id already exists
-            if(!file_exists(SVG_DIR . $basePath . $filename))
-            {
-                // get template by id
-                $template = $connector->getTemplateById($container->getId());
-
-                // create svg
-                $handle = fopen(SVG_DIR . '/' . $basePath  . '/' . $filename, 'w');
-                if(!$handle)
-                {
-                    throw new Exception('Could not open file ' . SVG_DIR . $filename);
-                }
-                fwrite($handle, $template->getSvgContent());
-                fclose($handle);
-            }
-
-        }
-        else
-        {
-            throw new Exception(SVG_DIR . '/' . $basePath . ' not found!');
-        }
-*/
 
         // view parameters
         $this->view->templateId = $container->getId();
@@ -93,11 +49,8 @@ class Editor extends Controller
         $this->view->cmeoRefOptions = $this->getCmeoRefOptions();
         $this->view->cmeoLinkOptions = $this->getCmeoLinkOptions();
 
-        if(!file_exists($container->getOutputDir() . '/' . $baseFilename . '.gif'))
-        {
-            $container->setTarget('GIF');
-            $container->render();
-        }
+        $container->setTarget('GIF');
+        $container->render();
 
         // unlink(SVG_DIR . $filename);
 
