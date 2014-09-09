b20d4087c3ad2d1ad2086f23d5280f4f00825a93
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-09-09 13:23:37 +0200

    add "global settings" sub template and inclusion of element to editor view

---
+++ views/globals.inc.php
@@ -0,0 +1,26 @@
+<div class="panel panel-default">
+    <a data-toggle="collapse" data-target=""
+       href="#globals">
+    <div class="panel-heading rectangleTitle">
+        <h3 class="panel-title">
+                Global settings:
+        </h3>
+    </div>
+    </a>
+    <div id="globals" class="panel-collapse collapse"
+    <div class="panel-body">
+        <div class="container-fluid">
+            <div class="row">
+                <?php // include('editorComponents/color.inc.php'); ?>
+            </div>
+            <div class="row">
+                <?php
+                include('editorComponents/dimensions.inc.php');
+                ?>
+            </div>
+            <div class="row">
+
+            </div>
+        </div>
+    </div>
+</div>
