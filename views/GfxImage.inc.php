95ae0c8b6484f5431da3498d5f74cf1e6251d93c
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-10-07 15:33:49 +0200

    move style definition to global css file, fix wrong value (pointer, not hand, the latter obviously isn't working in Firefox?)

--- views/GfxImage.inc.php
+++ views/GfxImage.inc.php
@@ -2,7 +2,7 @@
     <div class="panel-heading imageTitle">
         <h3 class="panel-title">
             Image: <?php echo str_replace('_', ' ', $element->getId());?>
-            <span id="<?php echo $element->getId();?>" class="glyphicon glyphicon-remove-circle" style="float:right;cursor:hand;"></span>
+            <span id="<?php echo $element->getId();?>" class="glyphicon glyphicon-remove-circle"></span>
         </h3>
     </div>
     <div class="panel-body">
