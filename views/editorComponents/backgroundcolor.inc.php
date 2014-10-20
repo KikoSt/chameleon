9beb22518f082c8b8f2e191685fa24c7334fb126
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-10-20 16:09:59 +0200

    add new components used for group editing

--- views/editorComponents/backgroundcolor.inc.php
+++ views/editorComponents/backgroundcolor.inc.php
@@ -1,11 +1,11 @@
 <div class="row">
-    <label class="col-md-3">Color:</label>
+    <label class="col-md-3">Bg color:</label>
     <div>
         <input id="fill"
                type="text"
                class="form-control picker"
                name="<?php echo $element->getId();?>#fill"
-               value="<?php echo (!empty($element->getFill()) ? $element->getFill()->getHex() : '#000000');?>"
+               value="<?php echo (!empty($element->getBackgroundcolor()) ? $element->getBackgroundcolor()->getHex() : '#000000');?>"
                placeholder="color"
                style="float:left;"
             />
---
+++ views/editorComponents/groupLink.inc.php
@@ -0,0 +1,12 @@
+<div class="row">
+    <label class="col-md-3" for="<?php echo $element->getId();?>#cmeoLink">Link:</label>
+    <select name="<?php echo $element->getId();?>#cmeoLink"
+            class="form-control"
+        >
+        <option value="">Select option...</option>
+        <?php foreach($this->cmeoLinkOptions as $option): ?>
+            <option value="<?php echo $option;?>" <?php echo ($option === $element->getLink()) ? 'selected' : '';?>><?php echo $option;
+                ?></option>
+        <?php endforeach;?>
+    </select>
+</div>
