584f396c41182904e2efb2842127f51dd124b8e1
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-10-20 16:11:05 +0200

    add group views

---
+++ views/GfxGroup.inc.php
@@ -0,0 +1,34 @@
+<div id="grouppanel_<?php echo $element->getId();?>" class="panel panel-default component">
+    <div class="panel-heading groupTitle">
+        <h3 class="panel-title">
+            Group: <?php echo str_replace('_', ' ', $element->getId());?>
+            <span id="<?php echo $element->getId();?>" class="glyphicon glyphicon-remove-circle"></span>
+        </h3>
+    </div>
+    <div class="panel-body">
+        <div class="container-fluid">
+            Group <?php echo $element->getId(); ?>:<br />
+            <?php
+                foreach($element->getElements() AS $elem)
+                {
+                    echo $elem->getId() . ' / ';
+                }
+            ?>
+            <br />
+            <br />
+        </div>
+        <div class="container-fluid">
+        <?php
+            if($this->premiumUser):
+                include('editorComponents/coords.inc.php');
+            endif;
+            include('editorComponents/foregroundcolor.inc.php');
+            include('editorComponents/backgroundcolor.inc.php');
+            include('editorComponents/text.inc.php');
+            include('editorComponents/fontFamily.inc.php');
+            include('editorComponents/groupSource.inc.php');
+            include('editorComponents/groupLink.inc.php');
+        ?>
+        </div>
+    </div>
+</div>
