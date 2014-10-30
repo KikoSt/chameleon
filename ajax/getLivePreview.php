32188571c254f28d4a55a1830349a37646aa2aef
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-10-30 12:30:46 +0100

    add lightbox plugin

--- ajax/getLivePreview.php
+++ ajax/getLivePreview.php
@@ -34,30 +34,70 @@ $connector->setCompanyId($companyId);
 $connector->setAdvertiserId($advertiserId);
 $connector->setAuditUserId($auditUserId);
 
-$basePath = (string) $companyId . '/' . (string) $advertiserId . '/0';
+$targetPath = (string) $companyId . '/' . (string) $advertiserId . '/preview/' . $templateId;
+
+$dir = '../output/' . $targetPath;
+
+if(!file_exists($dir))
+{
+    // set the current umask to 0777
+    $old = umask(0);
+    if(!mkdir($dir, 0777, true))
+    {
+        throw new Exception('Could not create directory ' . $dir);
+    }
+    // reset umask
+    umask($old);
+}
 
 $template = $connector->getTemplateById($templateId);
 
-$categoryIds = array(167187, 167715, 167811);
+
+
+$categoryIds = array(167463, 167187, 167715, 167811);
 $numSamples = 5;
 
-$productSamples = $connector->getProductDataSamples($categoryIds, $numSamples);
+$products = $connector->getProductDataSamples($categoryIds, $numSamples);
 
-var_dump($productSamples);
+$argv = array(null, $companyId, $advertiserId, null, $auditUserId);
 
-$products = json_decode($productSamples);
+$generator = new CMEOGenerator($argv);
+$generator->setTemplates(array($templateId));
+$generator->setCategories($categoryIds);
 
-echo "\n";
+$count = 0;
 
-foreach($products AS $sample)
+foreach($products AS $product)
 {
-    var_dump($sample);
-    echo "\n";
+    echo "\n" . ++$count . "\n";
+    $categoryId = $product->getCategoryId();
+    $generator->prepareLogfile($categoryId);
+    $generator->getContainer()->setCategoryId($categoryId);
+    $sourcePath = (string) $companyId . '/' . (string) $advertiserId . '/' . $categoryId;
+
+    $generator->getContainer()->setSource($template->getSvgContent());
+    $generator->getContainer()->setId($template->getBannerTemplateId());
+
+    // $generator->getContainer()->setOutputDir('0');
+    // $generator->getContainer()->setOutputName(getPreviewFileName($template));
+
+    try
+    {
+        $generator->getContainer()->parse();
+    }
+    catch(Exception $e)
+    {
+        $generator->logMessage('An error occured: ' . $e->getMessage() . "\n");
+        continue;
+    }
+
+    // move file ...
+    $sourceName = '../output/' . $sourcePath . '/' . $generator->getContainer()->getOutputFilename() . '.gif';
+    $targetName = '../output/' . $targetPath . '/' . $generator->getContainer()->getOutputFilename() . '.gif';
+    rename($sourceName, $targetName);
+    $imgsrc = 'output/' . $basePath . '/' . $generator->getContainer()->getOutputFilename();
+    echo $imgsrc . "\n";
+
+    $generator->render($product);
 }
-
-// var_dump($template);
-die();
-
-$products = $connector->getProductsByCategory();
-
 ?>
