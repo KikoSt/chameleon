4beb27e1ea24eedfc82decbcb79c7f64c3efaae6
Kiko Starkmann <christoph.starkmann@mediadecision.com>
2014-12-15 14:16:31 +0100

    change exceptions autoloader

--- Bootstrap.php
+++ Bootstrap.php
@@ -16,9 +16,9 @@ function __autoload($className)
     {
         require_once(__ROOT__ . 'libraries/interfaces/' . $className . '.interface.php');
     }
-    else if(file_exists(__ROOT__ . 'libraries/exception/' . $className . '.exception.php'))
+    else if(file_exists(__ROOT__ . 'libraries/exceptions/' . $className . '.exception.php'))
     {
-        require_once(__ROOT__ . 'libraries/exception/' . $className . '.exception.php');
+        require_once(__ROOT__ . 'libraries/exceptions/' . $className . '.exception.php');
     }
     else if(file_exists(__ROOT__ . 'controllers/' . $className . '.php'))
     {
